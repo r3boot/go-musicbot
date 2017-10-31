ea581d8963a7b27e35d98c79386a82d529cda090
Lex van Roon <lvanroon@bol.com>
2017-10-31 13:20:12 +0100

    Several fixes in js player
    Change-Id: I0c2f6ff566092d5bbd9a82e90f8a40413ffc0b19

--- webassets/js/player.js
+++ webassets/js/player.js
@@ -11,28 +11,23 @@ function StartWebSocket() {
 
     document.getElementById("idNext").onclick = function() {
         ws.send(JSON.stringify(nextMsg));
-        console.log("next: " + nextMsg)
     };
 
     document.getElementById("idBoo").onclick = function() {
         ws.send(JSON.stringify(booMsg));
-        console.log("boo");
     };
 
     document.getElementById("idTune").onclick = function () {
         ws.send(JSON.stringify(tuneMsg));
-        console.log("tune");
     };
 
     document.getElementById("idSearch").onclick = function() {
         var query = {"Operation":"play","Query":document.getElementById("idQuery").value};
         ws.send(JSON.stringify(query));
-        console.log("play")
     };
 
     document.getElementById("idPlaylist").onclick = function() {
         window.open("/playlist", "_blank");
-        console.log("playlist")
     }
 
     ws.onopen = function() {
@@ -48,16 +43,12 @@ function StartWebSocket() {
 
     ws.onmessage = function(e) {
         var r = JSON.parse(e.data);
-        console.log(r);
 
         switch (r.Pkt) {
             case "np_r":
                 var update = "" + r.Data.Title + "(" + r.Data.Duration + ")  " + r.Data.Rating + "/10";
 
                 nps.innerHTML = update;
-                break;
-            case "play_r":
-                console.log(r);
                 break;
             default:
                 console.log("Unknown message received: " + r.Pkt)
