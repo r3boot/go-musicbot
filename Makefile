f951fbc9188f07da43bc88f5dc3eb777bf96e8e3
Lex van Roon <r3boot@r3blog.nl>
2017-11-16 22:48:58 +0100

    Add id3tag command

--- Makefile
+++ Makefile
@@ -1,17 +1,23 @@
 TARGET = musicbot
+ID3TAG = id3tag
 
 BUILD_DIR = ./build
 PREFIX = /usr/local
 
-all: ${TARGET}
+all: ${TARGET} ${ID3TAG}
 
 ${TARGET}:
 	[[ -d "${BUILD_DIR}" ]] || mkdir -p "${BUILD_DIR}"
 	go build -v -o ${BUILD_DIR}/${TARGET} cmd/${TARGET}/${TARGET}.go
 
+${ID3TAG}:
+	[[ -d "${BUILD_DIR}" ]] || mkdir -p "${BUILD_DIR}"
+	go build -v -o ${BUILD_DIR}/${ID3TAG} cmd/${ID3TAG}/${ID3TAG}.go
+
 install:
 	install -o root -m 0644 config/musicbot.yaml /etc/musicbot.yaml
-	install -o root -m 0755 ${BUILD_DIR}/${TARGET} ${PREFIX}/bin/${TARGET}
+	install -o root -m 0755 ${BUILD_DIR}/${ID3TAG} ${PREFIX}/bin/${ID3TAG}
+	install -o root -m 0755 ${BUILD_DIR}/${ID3TAG} ${PREFIX}/bin/${ID3TAG}
 	install -d -o root -g wheel -m 0755 webassets /usr/local/share/musicbot
 
 clean:
---
+++ cmd/id3tag/id3tag.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/r3boot/go-musicbot/lib/logger"
+	"github.com/r3boot/go-musicbot/lib/mp3lib"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const (
+	D_DEBUG         = false
+	D_GET_RATING    = true
+	D_SET_RATING    = -1
+	D_BASEDIR       = "/music"
+	D_RECURSE       = true
+	D_USE_TIMESTAMP = false
+)
+
+var (
+	debug      = flag.Bool("D", D_DEBUG, "Enable debug mode")
+	getRating  = flag.Bool("lr", D_GET_RATING, "Show current rating")
+	setRating  = flag.Int("sr", D_SET_RATING, "Set the rating")
+	baseDir    = flag.String("d", D_BASEDIR, "Music directory")
+	MP3Library *mp3lib.MP3Library
+	Logger     *logger.Logger
+)
+
+func SetRating(fname string, newRating int) {
+	fullPath, err := filepath.Abs(fname)
+	if err != nil {
+		Logger.Fatalf("SetRating filepath.Abs: %v", err)
+	}
+
+	MP3Library.SetRating(fullPath, newRating)
+}
+
+func ShowRating(fname string) {
+	fullPath, err := filepath.Abs(fname)
+	if err != nil {
+		Logger.Fatalf("ShowRating filepath.Abs: %v", err)
+	}
+
+	rating := MP3Library.GetRating(fullPath)
+
+	fmt.Printf("%d %s\n", rating, fullPath)
+}
+
+func ShowRatingsForDir(dirname string) {
+	entries, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		Logger.Warningf("GetRatingsForDir ioutil.ReadDir: %v", err)
+		return
+	}
+
+	for _, entry := range entries {
+		target := dirname + "/" + entry.Name()
+		if entry.IsDir() {
+			ShowRatingsForDir(target)
+		} else {
+			ShowRating(target)
+		}
+	}
+}
+
+func init() {
+	flag.Parse()
+
+	Logger = logger.NewLogger(D_USE_TIMESTAMP, *debug)
+	MP3Library = mp3lib.NewMP3Library(Logger, *baseDir)
+
+	// No arguments passed
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	target := flag.Args()[0]
+	if *setRating != -1 {
+		SetRating(target, *setRating)
+	} else if *getRating {
+		fs, err := os.Stat(target)
+		if err != nil {
+			Logger.Fatalf("os.Stat: %v", err)
+		}
+
+		if fs.IsDir() {
+			ShowRatingsForDir(target)
+		} else {
+			ShowRating(target)
+		}
+	}
+}
