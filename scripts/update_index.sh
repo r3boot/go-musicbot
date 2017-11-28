5884a04097616b0479862e60ebe45111602c4cd3
Lex van Roon <r3boot@r3blog.nl>
2017-11-29 00:55:21 +0100

    Use mpd for nowplaying info

--- scripts/update_index.sh
+++ scripts/update_index.sh
@@ -8,7 +8,7 @@ OUTPUT='/usr/local/share/musicbot/index.html'
 RADIO_2600NL_OGG_LISTENERS=$(grep '/2600nl.ogg' ${PLAYLIST_LOG} | grep -v ^Binary | tail -1 | cut -d\| -f3)
 RADIO_2600NL_MP3_LISTENERS=$(grep '/2600nl.mp3' ${PLAYLIST_LOG} | grep -v ^Binary | tail -1 | cut -d\| -f3)
 RADIO_2600NL_LISTENERS=$(expr ${RADIO_2600NL_OGG_LISTENERS} + ${RADIO_2600NL_MP3_LISTENERS})
-RADIO_2600NL_NOWPLAYING="$(grep '/2600nl.ogg' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f4)"
+RADIO_2600NL_NOWPLAYING="$(mpc status | head -1)"
 
 cat "${TEMPLATE}" | sed \
   -e "s,%2600NL_NOWPLAYING%,${RADIO_2600NL_NOWPLAYING},g" \
