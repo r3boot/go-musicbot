4d1a0307cd07a7d147f32b65c7a1f1d5b5459497
Lex van Roon <r3boot@r3blog.nl>
2017-11-28 22:46:18 +0100

    Added main index for radio station

---
+++ scripts/update_index.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PLAYLIST_LOG='/var/log/icecast/playlist.log'
+
+TEMPLATE='/usr/local/share/musicbot/templates/index.html'
+OUTPUT='/usr/local/share/musicbot/index.html'
+
+RADIO_2600NL_OGG_LISTENERS=$(grep '/2600nl.ogg' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f3)
+RADIO_2600NL_MP3_LISTENERS=$(grep '/2600nl.mp3' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f3)
+RADIO_2600NL_LISTENERS=$(expr ${RADIO_2600NL_OGG_LISTENERS} + ${RADIO_2600NL_MP3_LISTENERS})
+RADIO_2600NL_NOWPLAYING="$(grep '/2600nl.mp3' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f4)"
+
+cat "${TEMPLATE}" | sed \
+  -e "s,%2600NL_NOWPLAYING%,${RADIO_2600NL_NOWPLAYING},g" \
+  -e "s,%2600NL_NUMLISTENERS%,${RADIO_2600NL_LISTENERS},g" \
+  > ${OUTPUT}.new && mv ${OUTPUT}.new ${OUTPUT}
