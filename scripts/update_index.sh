8eb6168fff3a0ee22b38ae2f68c7350b2e7bd1a9
Lex van Roon <r3boot@r3blog.nl>
2017-11-29 00:41:07 +0100

    Small fix to index update script

--- scripts/update_index.sh
+++ scripts/update_index.sh
@@ -5,8 +5,8 @@ PLAYLIST_LOG='/var/log/icecast/playlist.log'
 TEMPLATE='/usr/local/share/musicbot/templates/index.html'
 OUTPUT='/usr/local/share/musicbot/index.html'
 
-RADIO_2600NL_OGG_LISTENERS=$(grep '/2600nl.ogg' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f3)
-RADIO_2600NL_MP3_LISTENERS=$(grep '/2600nl.mp3' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f3)
+RADIO_2600NL_OGG_LISTENERS=$(grep '/2600nl.ogg' ${PLAYLIST_LOG} | grep -v ^Binary | tail -1 | cut -d\| -f3)
+RADIO_2600NL_MP3_LISTENERS=$(grep '/2600nl.mp3' ${PLAYLIST_LOG} | grep -v ^Binary | tail -1 | cut -d\| -f3)
 RADIO_2600NL_LISTENERS=$(expr ${RADIO_2600NL_OGG_LISTENERS} + ${RADIO_2600NL_MP3_LISTENERS})
 RADIO_2600NL_NOWPLAYING="$(grep '/2600nl.ogg' ${PLAYLIST_LOG} | tail -1 | cut -d\| -f4)"
 
